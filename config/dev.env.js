ee648f60aa6af8694d60416f7d535bc7439c7280
lloydst <lloyd.stumpel@gmail.com>
2018-03-06 10:50:36 +0100

    Update dev.env.js

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,5 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  MURI: '"mongodb://admin:admin@ds145750.mlab.com:45750/example"'
+  NODE_ENV: '"development"'
 })
