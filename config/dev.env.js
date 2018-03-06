0b09af11958ffd3de86365e77c0a349f9d181632
Lloyd Stumpel <lloyd.stumpel@poort80.nl>
2018-03-06 10:46:53 +0100

    a few examples

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,6 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  MURI: '"mongodb://admin:admin@ds145750.mlab.com:45750/example"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,5 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  MURI: '"mongodb://admin:admin@ds145750.mlab.com:45750/example"'
 }
